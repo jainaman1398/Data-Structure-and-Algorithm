342b18568811eb08495c093598667d71b83a3ae9
jainaman1398 <ajjain.aman13@gmail.com>
2017-04-03 14:23:33 +0530

    Create permutation of string in lexicographical order using STL.cpp

---
+++ permutation of string in lexicographical order using STL.cpp
@@ -0,0 +1,15 @@
+// Aman Jain
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    do{
+        cout<<s<<endl;
+    }
+ while(next_permutation(s.begin(),s.end()));
+ return 0;
+}
